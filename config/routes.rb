c43e56ac7d4a877c6e1c24fe0cf83530af6ede6d
Alexandre Mauhin <alexandremauhin@outlook.fr>
2023-03-07 12:01:26 +0100

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
-  get 'dashboards/show'
-  get 'exercices/index'
-  get 'exercices/show'
-  get 'users/index'
   devise_for :users
   root to: "pages#home"
-  resources :submissions, only: [:new, :create, :show] do
-    resources :reviews, only: [:create]
+
+  resources :submissions, only: [:show] do
+    resources :reviews, only: [:new, :create]
   end
+
+  resources :reviews, only: [:index, :show]
+
   resources :exercices, only: [:index, :show] do
-    resources :submissions, only: [:new, :create, :show]
+    resources :submissions, only: [:new, :create]
     resources :bookmarks, only: [:create, :destroy]
   end
+
   resources :users, only: [:index]
 end
