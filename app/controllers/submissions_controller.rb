22b5529c32e5858ba755808157499238b2a2d5fd
Alexandre Mauhin <alexandremauhin@outlook.fr>
2023-03-07 13:16:15 +0100

    fixed previous commit that sent old files along the schema naming update

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,6 +1,8 @@
 class SubmissionsController < ApplicationController
 
   def new
+    # needs upstream data from Exercices controller
+    @exercice = Exercice.find(params[:exercice_id])
   end
 
   def create
