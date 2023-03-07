ed4ed171ffa04c80c1f95038da6f6650ebf093cb
Alexandre Mauhin <alexandremauhin@outlook.fr>
2023-03-07 16:43:09 +0100

    started to implement submissions (thus code editor) with basic a=a verification based on exercice solution for testing purposes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,5 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    raise
   end
 end
--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -3,13 +3,31 @@ class SubmissionsController < ApplicationController
   def new
     # needs upstream data from Exercices controller (is what's next OK?)
     @exercice = Exercice.find(params[:exercice_id])
-    @user_attempt = ""
+    @submission = Submission.new
   end
 
   def create
+    @submission = Submission.new
+    @user_code = params[:submission][:user_code]
+    @exercice = Exercice.find(params[:exercice_id])
+    @solution = @exercice.solution
+    if @user_code == @solution
+      puts "\n*****user_code equals solution*****\n"
+      #@submission = Submission.new(submission_params)
+      redirect_to submission_path(:show)
+    else
+      puts "\n*****user_code NOT equals solution*****\n"
+      render :new
+    end
   end
 
   def show
+  end
+
+  private
+
+  def submission_params
+    return params.permit(:submission).require(:user_code) #created_at etc?
   end
 
 end
