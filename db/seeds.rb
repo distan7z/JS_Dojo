78b7426d81669a9960f17e9ebc7a3d9b4d2a42b6
Lucas Martinot <lgmperso@gmail.com>
2023-03-07 13:15:10 +0100

    Finished Seed and added country and nickname to user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning Database..."
+
+Exercice.destroy_all
+User.destroy_all
+Submission.destroy_all
+
+puts "Database cleaned!"
+
+puts "Creating users..."
+User.create(username: "test-user", email: "test@email.com", password: "123456", country: "France")
+
+puts "Creating exercices..."
+Exercice.create(title: "Exercice 1", details: "Details 1", instructions: "Instruction 1", exp: 10, testing_code: "", solution: "abcd")
+
+puts "Creating submissions..."
+Submission.create(attempts_count: 1, validation:"", user_id: User.first.id, exercice_id: Exercice.first.id)
+
+puts "Database seeded!"
