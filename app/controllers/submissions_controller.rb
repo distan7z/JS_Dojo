4d0ffe91b6536365108041c723490a5d8aa79e52
Alexandre Mauhin <alexandremauhin@outlook.fr>
2023-03-08 12:34:09 +0100

    added a submission edit system in order to keep track of user code editing -- user input is filtered with strong params

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -7,27 +7,65 @@ class SubmissionsController < ApplicationController
   end
 
   def create
-    @submission = Submission.new
     @user_code = params[:submission][:user_code]
     @exercice = Exercice.find(params[:exercice_id])
     @solution = @exercice.solution
-    if @user_code == @solution
-      puts "\n*****user_code equals solution*****\n"
-      #@submission = Submission.new(submission_params)
-      redirect_to submission_path(:show)
+
+    @submission = Submission.new(submission_params)
+
+    @submission.user_id = current_user.id
+    @submission.exercice_id = @exercice.id
+
+    puts "Errors:\n"
+    puts @submission.errors.full_messages
+
+    puts "_____________________________________________________________________"
+
+    if @submission.save
+      puts "Submission successfully saved!\n Redirecting to edit_exercice path"
+      redirect_to edit_exercice_submission_path(@exercice, @submission) # pour éviter de réécrire la validation dans create ET edit
     else
-      puts "\n*****user_code NOT equals solution*****\n"
-      render :new
+      puts "Submission not saved!\n Redirecting to new_exercice path"
+      redirect_to new_exercice_submission_path(@exercice)
     end
+    puts "_____________________________________________________________________\n"
   end
 
   def show
   end
 
+  def edit
+    @submission = Submission.find(params[:id])
+    @exercice = Exercice.find(@submission.exercice_id)
+    puts "_____________________________________________________________________"
+    if @submission.user_code == @exercice.solution
+      puts "\n*****user_code equals solution*****\n"
+      redirect_to submission_path(:show)
+    else
+      puts "\n*****user_code NOT equals solution*****\n"
+      render :edit
+    end
+    puts "_____________________________________________________________________\n"
+  end
+
+  def update
+    @submission = Submission.find(params[:id])
+    @exercice = Exercice.find(@submission.exercice_id)
+    @submission.user_code = submission_params[:user_code]
+    if @submission.save
+      puts "Submission successfully updated!\n Redirecting to edit_exercice path"
+      redirect_to edit_exercice_submission_path(@exercice, @submission)
+    else
+      puts "Submission not updated!\n Redirecting to new_exercice path"
+      redirect_to edit_exercice_submission_path(@exercice)
+    end
+    puts "_____________________________________________________________________\n"
+  end
+
   private
 
   def submission_params
-    return params.permit(:submission).require(:user_code) #created_at etc?
+    return params.require(:submission).permit(:user_code) #created_at etc?
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
     resources :reviews, only: [:create]
   end
 
-  resources :exercices, only: [:index, :show], shallow: true do
-    resources :submissions, only: [:new, :create]
+  resources :exercices, only: [:index, :show] do
+    resources :submissions, only: [:new, :create, :edit, :update]
     resources :bookmarks, only: [:create, :destroy]
   end
 
