598ebe5fb70a655d5e44fb4b54dced637020142b
Lucas Martinot <lgmperso@gmail.com>
2023-03-08 18:06:24 +0100

    added profile picture and profile banner

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :country])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :country, :avatar])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :country])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :country, :avatar, :banner_picture])
   end
 end
