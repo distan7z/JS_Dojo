ddd21eb2ac639f9ee6f6e289e2cdf9625fbc350a
Lucas Averty <lu.averty@hotmail.fr>
2023-03-14 15:29:11 +0100

    TO DO : Not finish the achievement method on User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,27 +26,36 @@ class User < ApplicationRecord
     submissions.validated.where(attempts_count: 1).any?
   end
 
-  def completion_achievements
-   #si current_user réussit 10, 50, 100 exercices.
-   #alors il décroche un badge
+  def leaderboard_position
+
   end
 
-  def accuracy_achievements
-    # submissions.validated.where(attempts_count: 1)
+
+  def completion_achievements(user)
+    #si current_user réussit 10, 50, 100 exercices.
+    #alors il décroche un Achievement
+    exercises_completed = user.attempts.distinct.count(:exercise_id)
+  end
+
+  end
+
+  def accuracy_achievements(user)
     #si current_user réussit 10 exercices avec un attempts_count: 1
-    #alors il décroche un badge
+    #alors il décroche un Achievement
+     exercises = user.attempts.where(attempts_count: 1).distinct(:exercise_id)
+     exercises.count >= 10
   end
 
-  def leaderboard_achievements_15
+  def leaderboard_achievements_15(user)
     #si current_user réussit a se classer dans les 15 premiers
-    #alors il décroche un badge
+    #alors il décroche un Achievement
+     user.leaderboard_position <= 15
   end
 
-  def badge_achievements
-    #si current_user réussit a avoir les badges suivants
-    #alors il décroche un badge
+  def badge_achievements(user)
+    #si current_user réussit a avoir les Achievement précédent
+    #alors il décroche un Achievement
   end
-
   private
 
   def user_params
