502e8601eff388e3a9d43e2b52e367b104a6e7af
Lucas Martinot <lgmperso@gmail.com>
2023-03-14 09:35:26 +0100

    fixed login status, small tweaks to dashboard view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    if user_logged_in?
+    if user_signed_in?
       redirect_to dashboards_path
     end
   end
