1f06a0113e74c949d53b34b0ecfa55cdcffa555a
Alexandre Mauhin <alexandremauhin@outlook.fr>
2023-03-14 14:41:48 +0100

    enhance code editor front-end (validations counter next to button)

--- app/javascript/controllers/editor_controller.js
+++ app/javascript/controllers/editor_controller.js
@@ -3,17 +3,9 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="editor"
 export default class extends Controller {
   static targets = ["editor"]
-  submit_code() {
-    let unclean_code = this.editorTarget.innerText;
-    let clean_code = "";
-
-    /* removing code editor lines */
-    for (var i = 0; i < unclean_code.length; i++) {
-      if(isNaN(unclean_code.charAt(i))) {
-        clean_code += unclean_code.charAt(i);
-      }
-    }
-
-    console.log(clean_code)
+  submit_code(e) {
+    e.preventDefault;
+    console.log("oe");
+    let editor_code = this.editorTarget.querySelector(".ace_content").innerText;
   }
 }
