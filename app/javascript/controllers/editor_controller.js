8ac908cb2adc347feee90acf30476c263a2805d9
Alexandre Mauhin <alexandremauhin@outlook.fr>
2023-03-14 15:19:11 +0100

    linked stimulus with ruby controller and added a saving user code system

--- app/javascript/controllers/editor_controller.js
+++ app/javascript/controllers/editor_controller.js
@@ -2,10 +2,13 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="editor"
 export default class extends Controller {
-  static targets = ["editor"]
+  static targets = ["editor", "input"]
   submit_code(e) {
     e.preventDefault;
     console.log("oe");
     let editor_code = this.editorTarget.querySelector(".ace_content").innerText;
+    console.log(`editor_code: ${editor_code}`);
+    console.log(`this.inputTarget.value: ${this.inputTarget.value}`);
+    this.inputTarget.value = editor_code
   }
 }
