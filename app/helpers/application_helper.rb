07438d1a5f0bb61e9d6beabd8619dfc5cf064480
Lucas Martinot <lgmperso@gmail.com>
2023-03-15 15:20:59 +0100

    added ordinal_indicator

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def ordinal_indicator(number)
+    if [11, 12, 13].include?(number % 100)
+      return "#{number}th"
+    else
+      case number % 10
+      when 1; "#{number}st"
+      when 2; "#{number}nd"
+      when 3; "#{number}rd"
+      else    "#{number}th"
+      end
+    end
+  end
 end
