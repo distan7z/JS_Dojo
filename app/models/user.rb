36a089bd837623120ef4d971585a864c8f308c3b
Lucas Martinot <lgmperso@gmail.com>
2023-03-17 13:18:26 +0100

    add validate on model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@ class User < ApplicationRecord
 
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
+  validates :country, presence: true
 
 
   def total_experience
